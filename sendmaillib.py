04093436f3bb339c49cb38a8ea60d75f32e1c22c
KaiLuZheng <lumen_o@126.com>
2018-10-12 16:45:27 +0800

    updata

--- sendmaillib.py
+++ sendmaillib.py
@@ -7,13 +7,12 @@ from email.header import Header
 import logging
  
 sender = 'monitor@min.com'
-revs = ['912279158@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+revs = ['912279158@qq.com']  # address
  
 def sendEmail(body, receivers = revs):
-    # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
     message = MIMEText(body, 'plain', 'utf-8')
-    message['From'] = Header("monitor", 'utf-8')    # 发送者
-    message['To'] =  Header("reader", 'utf-8')      # 接收者
+    message['From'] = Header("monitor", 'utf-8')    # sender
+    message['To'] =  Header("reader", 'utf-8')      # receiver
  
     subject = 'check data'
     message['Subject'] = Header(subject, 'utf-8')
@@ -22,10 +21,10 @@ def sendEmail(body, receivers = revs):
         smtpObj = smtplib.SMTP('localhost')
         logging.info(message)
         smtpObj.sendmail(sender, receivers, message.as_string())
-        logging.info("邮件发送成功")
+        logging.info("mail send succeeded.")
     except smtplib.SMTPException as e:
         logging.error(e)
-        logging.error("Error: 无法发送邮件")
+        logging.error("Error: mail send error")
 
 
 if __name__ == '__main__':
